4c8f3b21ebd7836140606731a490ece5af0a0667
Gabryella Santos <gabryellasantospo@gmail.com>
2026-04-05 22:32:05 -0300

    Mudança: alteração no nome arquivo MainActivity.kt que agora se chama TelaInicial.kt

--- app/src/main/java/com/example/taskcheck/ui/TelaInicial.kt
+++ app/src/main/java/com/example/taskcheck/ui/TelaInicial.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.taskcheck.R
 
-class MainActivity : AppCompatActivity() {
+class TelaInicial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
